f5eeecf248fb4fcb8e031c14b47ffe0cbd0e5905
thedavidmeister <thedavidmeister@gmail.com>
2025-01-29 20:35:01 +0400

    add foundry

--- flake.nix
+++ flake.nix
@@ -4,18 +4,20 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    foundry.url = "github:shazow/foundry.nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, foundry }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        overlays =[ foundry.overlay ];
         pkgs = import nixpkgs {
-          inherit system;
+          inherit system overlays;
         };
       in {
         pkgs = pkgs;
         devShells.default = pkgs.mkShell {
-          buildInputs = [ pkgs.yarn ];
+          buildInputs = [ pkgs.yarn pkgs.foundry-bin ];
         };
       });
 }
